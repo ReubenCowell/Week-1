4ee24236d1cce7e9d634dbfbc0875193bbb9ce02
Reuben Cowell <reuben.reubencowell.cowell@gmail.com>
2021-08-06 17:31:21 +0100

    added the functions to read a message using the end of transmission character

--- using_sockets_in_python/client_socket.py
+++ using_sockets_in_python/client_socket.py
@@ -1,40 +1,42 @@
 # you can use this to connect to the server socket in socket_server.py
 
-import socket  # imports the socket module
+import socket  # imports the socket module so we can connect to the server
 
 
-def get_text(receiving_socket):  # to receive the message
-    buffer = ""
+def get_text(receiving_socket):  # a function to receive the message
+    buffer = ""  # this buffer is used to append the message to it as it is received
 
-    socket_open = True
+    socket_open = True  # sets up the while loop
     while socket_open:
         # read any data from the socket
-        data = receiving_socket.recv(1024)
+        data = receiving_socket.recv(1024)  # gets the receiving socket (server_socket) and calls the built in recv
+        # function to receive 1024 bytes of data, which is the maximum amount of data that can be received
 
         # if no data is returned the socket must be closed
         if not data:
-            socket_open = False
+            socket_open = False  # stops the while loop
 
         # add the data to the buffer
-        buffer = buffer + data.decode()
+        buffer = buffer + data.decode()  # adds the decoded data through UTF-8 to the buffer string variable
 
         # is there a terminator in the buffer
-        terminator_pos = buffer.find("\n")
+        terminator_pos = buffer.find("\n")  # uses python's built in function to find the terminating character '\n'
         # if the value is greater than -1, a \n must exist
-        while terminator_pos > -1:
+        while terminator_pos > -1:  # if string.find() returns -1, '\n' could # not be found
             # get the message from the buffer
             message = buffer[:terminator_pos]
             # remove the message from the buffer
-            buffer = buffer[terminator_pos + 1:]
-            # yield the message (see below)
-            yield message
+            buffer = buffer[terminator_pos + 1:]  # takes off the \n
+            # yield the messages, like return
+            yield message  # yield is used to return certain kinds of functions, generators
             # is there another terminator in the buffer
             terminator_pos = buffer.find("\n")
+        socket_open = False
 
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # sets up a socket with ip and TCP, the same as
 # the server one
-client_socket.connect(("127.0.0.1", 8081))  # connects to the ip and port
+client_socket.connect(("127.0.0.1", 8081))  # connects to the ip and port of the server
 print("Connected")
 
 """
@@ -44,11 +46,22 @@ data = client_socket.recv(1024)  # data is received and added to the data variab
 message = data.decode()  # decodes the message
 print(message)  # prints the message
 """
-for message in get_text(client_socket):
+
+print('Message received:\n ')
+for message in get_text(client_socket):  # prints the message using the get_text function
     print(message)
 
+"""
+you could also use next to call get_text once and get a single message:
+
+message = next(get_text(client_socket))
+print(message)
+
+"""
+
+""" sends a message back to the server:
 message = "message received"
 data = message.encode()
 client_socket.send(data)
-
+"""
 client_socket.close()  # it is good practice to close sockets
--- using_sockets_in_python/server_socket.py
+++ using_sockets_in_python/server_socket.py
@@ -2,31 +2,35 @@ import socket  # to use the socket api, import the module
 
 
 def send_text(sending_socket, text):  # this adds a terminating character to the string and sends
-    text = text + "\n"
-    data = text.encode()
-    sending_socket.send(data)
+    text = text + "\n"  # appends the "\n" terminating character to the string
+    data = text.encode()  # encodes the string using UTF-8
+    sending_socket.send(data)  # sends the string using the built in function for sending data
 
 
-message = input('what do you want to send?')
+message = input('what do you want to send?')  # gets the message that is to be sent to the client
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # creates a socket
+# the parameter AF_INET is to use ip and SOCK_STREAM tells it to use TCP
 
-# AF_INET to use ip and SOCK_STREAM tells it to use TCP
 server_socket.bind(("0.0.0.0", 8081))  # binds the socket to a ip, 0.0.0.0 and the port 8081 as it is used for testing
-server_socket.listen()  # tells it to listen for a connection to be made
+server_socket.listen(0)  # tells it to listen for a connection to be made
 print("Waiting for connection")
-connection_socket, address = server_socket.accept()  # if you put 127.0.0.0:8081 into a web browser,
+
+connection_socket, address = server_socket.accept()  # if you put 127.0.0.1:8081 into a web browser, it will not
+# display anything, but this will be seen as connected,  172.0.0.1  is the local port of this computer
+print("connected")
+
 """Ports are typically used to identify the purpose of the connection and can be any number between 0 and 65535. 
 Ports 0 to 1023 are well known ports that are usually restricted for specific uses: for example, port 80 is HTTP and 
 should not really be used. """
-# it appears as connected, 17.0.0.0 is the local port of this computer
-print('connected')
 
 send_text(connection_socket, message)  # calls the send function that also adds a end of transmission character
 
+""" this works, it receives a message back from the client
 msg = connection_socket.recv(1024)
 message = msg.decode()
 print(message)
+"""
 
-# it is good practice to close sockets:
+# it is good practice to close the sockets:
 connection_socket.close()
 server_socket.close()
